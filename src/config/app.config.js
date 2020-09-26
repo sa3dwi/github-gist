7d770c8238e3ba3f31ddf4b83acbecae942649a2
Ahmed <ahmed.magdy@intigral.net>
2020-09-27 00:38:03 +0400

    Add fallback to app.config

--- src/config/app.config.js
+++ src/config/app.config.js
@@ -1,8 +1,8 @@
 import config from "./../config.json";
 
 export const appConfig = {
-    API_GITHUB_SEARCH: config.api_github_search,
-    MIN_SEARCH_CHARACTERS: config.min_search_characters,
-    MAX_SEARCH_CHARACTERS: config.max_search_characters,
-    LOCAL_STORAGE_KEY: config.local_storage_key
+    API_GITHUB_SEARCH: config.api_github_search || "https://api.github.com/users/{username}/gists",
+    MIN_SEARCH_CHARACTERS: config.min_search_characters || 2,
+    MAX_SEARCH_CHARACTERS: config.max_search_characters || 50,
+    LOCAL_STORAGE_KEY: config.local_storage_key || "gistresentsearch"
 };
